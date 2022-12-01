c6608e92741883c8f6495e248347071f7ce89781
KimNayoun <94847818+NayounK1m@users.noreply.github.com>
2022-12-01 10:14:38 -0500

    Mapping Done

--- Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -16,13 +16,20 @@
 		string[] _locationStrings = new string[3];
 		Vector2d[] _locations;
 
+		[Geocode]
+		string[] _locationCoyoteStrings = new string[5];
+		Vector2d[] _locationsCoyote;
+
 		[SerializeField]
-		float _spawnScale = 100f;
+		float _spawnScale = 1f;
 
 		[SerializeField]
 		GameObject _markerPrefab;
+		[SerializeField]
+		GameObject _coyotePrefab;
 
 		List<GameObject> _spawnedObjects;
+		List<GameObject> _spawnedCoyoteObjects;
 
 		void Start()
 		{
@@ -38,17 +45,42 @@
 				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
 				_spawnedObjects.Add(instance);
 			}
-		}
+
+			//코요태
+			_locationsCoyote = new Vector2d[_locationCoyoteStrings.Length];
+			_spawnedCoyoteObjects = new List<GameObject>();
+			for (int i = 0; i < _locationCoyoteStrings.Length; i++)
+            {
+                var locationString = SingletonLatLng.instance.CoyoteLat[i] + "," + SingletonLatLng.instance.CoyoteLng[i];
+                Debug.Log(locationString);
+				_locationsCoyote[i] = Conversions.StringToLatLon(locationString);
+                Debug.Log(_locationsCoyote[i]);
+                var instance = Instantiate(_coyotePrefab);
+                instance.transform.localPosition = _map.GeoToWorldPosition(new Vector2d(5, 20), true);
+                Debug.Log(instance.transform.localPosition);
+                instance.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+				_spawnedCoyoteObjects.Add(instance);
+            }
+        }
 
 		private void Update()
 		{
-			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
+            int count = _spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = _spawnedObjects[i];
+                var location = _locations[i];
+                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            }
+
+			int countCoyote = _spawnedCoyoteObjects.Count;
+			for (int i = 0; i < countCoyote; i++)
 			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+				var spawnedCoyoteObject = _spawnedCoyoteObjects[i];
+				var locationCoyote = _locationsCoyote[i];
+				spawnedCoyoteObject.transform.localPosition = _map.GeoToWorldPosition(locationCoyote, true);
+				spawnedCoyoteObject.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
 			}
 		}
 	}
